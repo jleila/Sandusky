78e5572863d57461c608e5e362f3446aab7ef694
Julia Bouzaher <julia.bouzaher@gmail.com>
2021-02-22 19:44:39 -0800

    data organization reformatting

---
+++ README.txt
@@ -0,0 +1,7 @@
+Instructions:
+
+1) To replicate data processing, shapefile pre-processing, and run the Shiny App locally, click on Sandusky.Rproj. Once R has opened, go to the bottom right "Files" window, open final_data_processing.Rmd and shapefile_processing.Rmd. You will run the files in that order. Click "run all" at the top with the green triangle. App.R can be run at any time. Click "run" at the top with the green triangle.
+
+
+2) The directory as available here is slightly different than the directory bundle as uploaded as uploaded to RShinyApps.io. For reference (if the app or basis needs to be altered and reuploaded) that bundle differs from the existing directory as shown here by deleting the "external" folder (located at data/source/external) and the "Sandusky Bay Pathway" folder (located at data/source/city)
+
--- app.R
+++ app.R
@@ -1,13 +1,41 @@
+library(data.table)
 library(shiny)
-library(leaflet)
-library(dplyr)
-library(RColorBrewer)
+library(sf)
 library(sp)
-library(shinythemes)
+library(raster)
+library(leaflet)
 library(dashboardthemes)
 library(shinydashboard)
-library(diffobj)
-library(shinydashboardPlus)
+
+leafIcons <- icons(
+  iconUrl = 
+    "http://leafletjs.com/examples/custom-icons/leaf-green.png",
+  iconWidth = 15, iconHeight = 40,
+  iconAnchorX = 14, iconAnchorY = 40
+)
+
+labels2<- c("Developed - Medium/High Intensity","Developed - Low/Medium Intensity","Developed - Forested", "Developed - herbaceous or shrubland", "Cropland","Tree Cover 0-50%", "Tree Cover 50-100%","Herb Cover 0-50%", "Herb Cover 50-100%", "NA")
+final <- read_sf("data/derived/final.shp")
+Wetlands3 <- read_sf("data/derived/Wetlands3.shp")
+Neighborhoods <- read_sf("data/source/city/Neighborhoods/City_of_Sandusky_Neighborhoods.shp")
+# Neighborhoods <- st_transform(Neighborhoods, 4326)
+GLRI <- read_sf("data/source/city/GLRI Pathway/Polygon_layer.shp")
+# GLRI <- st_transform(GLRI, 4326)
+parks <- fread(file="data/derived/parks.txt")
+
+test_2001 <- raster::raster("data/derived/ras_2001.gri") 
+test_2008 <- raster::raster("data/derived/ras_2008.gri") 
+test_2010 <- raster::raster("data/derived/ras_2010.gri") 
+test_2012 <- raster::raster("data/derived/ras_2012.gri") 
+test_2014 <- raster::raster("data/derived/ras_2014.gri")
+test_2016<- raster::raster("data/derived/ras_2016.gri")
+
+pal1<- leaflet::colorFactor(palette = "Spectral", domain = values(test_2016), na.color = "transparent")
+pal2 <- leaflet::colorFactor(palette = "Spectral", domain = values(test_2014), na.color = "transparent")
+pal3 <- leaflet::colorFactor(palette = "Spectral", domain = values(test_2012), na.color = "transparent")
+pal4 <- leaflet::colorFactor(palette = "Spectral", domain = values(test_2010), na.color = "transparent")
+pal5 <- leaflet::colorFactor(palette = "Spectral", domain = values(test_2008), na.color = "transparent")
+pal6 <- leaflet::colorFactor(palette = "Spectral", domain = values(test_2001), na.color = "transparent")
 
 
 
@@ -73,7 +101,7 @@ server <- function(input, output, session) {
   
   
   output$map <- renderLeaflet({
-      pal <- colorFactor(rainbow(229), domain = values(landcover2016)) %>%
+      pal <- colorFactor(rainbow(229), domain = values(test_2016)) %>%
     
       leaflet() %>%
     
@@ -93,12 +121,12 @@ server <- function(input, output, session) {
         
       addMarkers(data = parks, icon = leafIcons, group = "Parks", popup = ~(FEATURE_NAME)) %>%
         
-        addRasterImage(x, pal1, opacity = 1, group = "Land Cover 2016") %>%
-        addRasterImage(xx, pal2, opacity = 1, group = "Land Cover 2014") %>%
-        addRasterImage(xxx, pal3, opacity = 1, group = "Land Cover 2012") %>%
-        addRasterImage(xxxx, pal4, opacity = 1, group = "Land Cover 2010") %>%
-        addRasterImage(xxxxx, pal5, opacity = 1, group = "Land Cover 2008") %>%
-        addRasterImage(xxxxxx, pal6, opacity = 1, group = "Land Cover 2001")
+        addRasterImage(test_2016, pal1, opacity = 1, group = "Land Cover 2016") %>%
+        addRasterImage(test_2014, pal2, opacity = 1, group = "Land Cover 2014") %>%
+        addRasterImage(test_2012, pal3, opacity = 1, group = "Land Cover 2012") %>%
+        addRasterImage(test_2010, pal4, opacity = 1, group = "Land Cover 2010") %>%
+        addRasterImage(test_2008, pal5, opacity = 1, group = "Land Cover 2008") %>%
+        addRasterImage(test_2001, pal6, opacity = 1, group = "Land Cover 2001")
       
   })
   
@@ -120,7 +148,7 @@ server <- function(input, output, session) {
                   highlightOptions = highlightOptions(color = "white",
                                                       weight = 2,
                                                       bringToFront = TRUE)) %>%
-      addLegend(pal = pal4, opacity = 1, values = values(xxxx), labFormat = function(type, cuts, p) {paste0(labels2)}, group = "Land Cover Legend", title = "Land Cover Legend") %>%
+      addLegend(pal = pal4, opacity = 1, values = values(test_2010), labFormat = function(type, cuts, p) {paste0(labels2)}, group = "Land Cover Legend", title = "Land Cover Legend") %>%
       addLegend(position ="bottomleft", pal = pal, group = "Display Census Data", na.label = "N/A for Current Year", labFormat = labelFormat(transform = function(x) sort(x, decreasing = TRUE)), values = ~filteredData()[[1]], opacity=1, title = "Census Data Legend") %>%
       addLayersControl(
         overlayGroups = c("Display Census Data","Parks","Wetlands","Land Cover Legend", "Downtown Neighborhood Lines","Sandusky Bay Pathway","Land Cover 2001", "Land Cover 2008", "Land Cover 2010", "Land Cover 2012", "Land Cover 2014", "Land Cover 2016"),
