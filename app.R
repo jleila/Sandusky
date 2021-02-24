2862c77756fa3568e62b3e8a82db9531e37e39f6
Julia Bouzaher <julia.bouzaher@gmail.com>
2021-02-23 20:03:13 -0800

    completing descriptive code comments

--- app.R
+++ app.R
@@ -1,3 +1,6 @@
+
+#load packages 
+
 library(data.table)
 library(shiny)
 library(sf)
@@ -6,6 +9,8 @@ library(raster)
 library(leaflet)
 library(dashboardthemes)
 library(shinydashboard)
+
+#load necessary data and UI accessories
 
 leafIcons <- icons(
   iconUrl = 
@@ -23,27 +28,25 @@ GLRI <- read_sf("data/source/city/GLRI Pathway/Polygon_layer.shp")
 # GLRI <- st_transform(GLRI, 4326)
 parks <- fread(file="data/derived/parks.txt")
 
-test_2001 <- raster::raster("data/derived/ras_2001.gri") 
-test_2008 <- raster::raster("data/derived/ras_2008.gri") 
-test_2010 <- raster::raster("data/derived/ras_2010.gri") 
-test_2012 <- raster::raster("data/derived/ras_2012.gri") 
-test_2014 <- raster::raster("data/derived/ras_2014.gri")
-test_2016<- raster::raster("data/derived/ras_2016.gri")
+ras_2001 <- raster::raster("data/derived/ras_2001.gri") 
+ras_2008 <- raster::raster("data/derived/ras_2008.gri") 
+ras_2010 <- raster::raster("data/derived/ras_2010.gri") 
+ras_2012 <- raster::raster("data/derived/ras_2012.gri") 
+ras_2014 <- raster::raster("data/derived/ras_2014.gri")
+ras_2016<- raster::raster("data/derived/ras_2016.gri")
 
-pal1<- leaflet::colorFactor(palette = "Spectral", domain = values(test_2016), na.color = "transparent")
-pal2 <- leaflet::colorFactor(palette = "Spectral", domain = values(test_2014), na.color = "transparent")
-pal3 <- leaflet::colorFactor(palette = "Spectral", domain = values(test_2012), na.color = "transparent")
-pal4 <- leaflet::colorFactor(palette = "Spectral", domain = values(test_2010), na.color = "transparent")
-pal5 <- leaflet::colorFactor(palette = "Spectral", domain = values(test_2008), na.color = "transparent")
-pal6 <- leaflet::colorFactor(palette = "Spectral", domain = values(test_2001), na.color = "transparent")
-
+pal1<- leaflet::colorFactor(palette = "Spectral", domain = values(ras_2016), na.color = "transparent")
+pal2 <- leaflet::colorFactor(palette = "Spectral", domain = values(ras_2014), na.color = "transparent")
+pal3 <- leaflet::colorFactor(palette = "Spectral", domain = values(ras_2012), na.color = "transparent")
+pal4 <- leaflet::colorFactor(palette = "Spectral", domain = values(ras_2010), na.color = "transparent")
+pal5 <- leaflet::colorFactor(palette = "Spectral", domain = values(ras_2008), na.color = "transparent")
+pal6 <- leaflet::colorFactor(palette = "Spectral", domain = values(ras_2001), na.color = "transparent")
 
 
 #header
 header <- dashboardHeader(title = "Sandusky, OH Demographic and Environmental Explorer", titleWidth = "1520px")
 
 #body
-    
 body <- dashboardBody(
    # shinyDashboardThemes(
    #   theme = "poor_mans_flatly"
@@ -78,7 +81,7 @@ sidebar <- dashboardSidebar(
   )
 )
 
-
+#ui call that hamburgers everything - header, sidebar, body - together
 
 ui <- dashboardPage(
   header,
@@ -86,22 +89,27 @@ ui <- dashboardPage(
   body
 )
 
+#server side where all of the actual mapping arrangements are made
   
 server <- function(input, output, session) {
-  
+  #creating a reactive dataframe from the main dataset (final)
   filteredData <- reactive({
     final %>%
+      #filtering based on user year selection
       filter(year == input$year) %>%
+      #filtering based on user Census variable selection
       dplyr::select(input$variable, tract)
       }) 
   
+  #generating reactive color palette
   colorpal <- reactive({
     colorNumeric("YlGnBu", filteredData()$input$variable, na.color = "#808080", reverse = TRUE)
     })
   
-  
+  #generating the static part of the map
+  #groups enable the checkbox layer display feature once called to addLayersControl
   output$map <- renderLeaflet({
-      pal <- colorFactor(rainbow(229), domain = values(test_2016)) %>%
+      pal <- colorFactor(rainbow(229), domain = values(ras_2016)) %>%
     
       leaflet() %>%
     
@@ -121,17 +129,17 @@ server <- function(input, output, session) {
         
       addMarkers(data = parks, icon = leafIcons, group = "Parks", popup = ~(FEATURE_NAME)) %>%
         
-        addRasterImage(test_2016, pal1, opacity = 1, group = "Land Cover 2016") %>%
-        addRasterImage(test_2014, pal2, opacity = 1, group = "Land Cover 2014") %>%
-        addRasterImage(test_2012, pal3, opacity = 1, group = "Land Cover 2012") %>%
-        addRasterImage(test_2010, pal4, opacity = 1, group = "Land Cover 2010") %>%
-        addRasterImage(test_2008, pal5, opacity = 1, group = "Land Cover 2008") %>%
-        addRasterImage(test_2001, pal6, opacity = 1, group = "Land Cover 2001")
+        addRasterImage(ras_2016, pal1, opacity = 1, group = "Land Cover 2016") %>%
+        addRasterImage(ras_2014, pal2, opacity = 1, group = "Land Cover 2014") %>%
+        addRasterImage(ras_2012, pal3, opacity = 1, group = "Land Cover 2012") %>%
+        addRasterImage(ras_2010, pal4, opacity = 1, group = "Land Cover 2010") %>%
+        addRasterImage(ras_2008, pal5, opacity = 1, group = "Land Cover 2008") %>%
+        addRasterImage(ras_2001, pal6, opacity = 1, group = "Land Cover 2001")
       
   })
   
   
-  
+  #the reactive part of the map
   
   observe({
     pal <- colorpal()
@@ -148,7 +156,7 @@ server <- function(input, output, session) {
                   highlightOptions = highlightOptions(color = "white",
                                                       weight = 2,
                                                       bringToFront = TRUE)) %>%
-      addLegend(pal = pal4, opacity = 1, values = values(test_2010), labFormat = function(type, cuts, p) {paste0(labels2)}, group = "Land Cover Legend", title = "Land Cover Legend") %>%
+      addLegend(pal = pal4, opacity = 1, values = values(ras_2010), labFormat = function(type, cuts, p) {paste0(labels2)}, group = "Land Cover Legend", title = "Land Cover Legend") %>%
       addLegend(position ="bottomleft", pal = pal, group = "Display Census Data", na.label = "N/A for Current Year", labFormat = labelFormat(transform = function(x) sort(x, decreasing = TRUE)), values = ~filteredData()[[1]], opacity=1, title = "Census Data Legend") %>%
       addLayersControl(
         overlayGroups = c("Display Census Data","Parks","Wetlands","Land Cover Legend", "Downtown Neighborhood Lines","Sandusky Bay Pathway","Land Cover 2001", "Land Cover 2008", "Land Cover 2010", "Land Cover 2012", "Land Cover 2014", "Land Cover 2016"),
@@ -160,6 +168,8 @@ server <- function(input, output, session) {
     })
   
 }  
+
+#call the shiny app to function
 
 shinyApp(ui, server)
 
